4dae3fb8943f5e1b474245ec19b76795b225824f
Surbhi Saraogi <surbhi.saraogi@treebohotels.com>
2019-01-15 12:46:51 +0530

    Modified setup file

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ import os
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 def get_version():
     basedir = os.path.dirname(__file__)
@@ -17,15 +20,20 @@ def get_version():
 setup(
     name='easy_logging',
     version=get_version(),
-    packages=find_packages(exclude=['tests', 'samples']),
-    install_requires=['logstash-formatter'],
-    url='',
-    license='MIT',
+    description='An easy to use logging library',
+    long_description=long_description,
     author='Surbhi Saraogi',
     author_email='surbhi.saraogi95@gmail.com',
+    long_description_content_type="text/markdown",
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=['tests', 'samples']),
+    install_requires=['logstash-formatter'],
+    url='https://github.com/Surbhi-1206/easy_logging',
     entry_points={
     },
-    test_suite="tests",
-    description='An easy to use logging library that provides a basic logging configuration which can be integrated '
-                'with any python application.'
 )
